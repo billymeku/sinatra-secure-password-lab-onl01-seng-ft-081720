2654066bbacffa7c47f2f351584c0b1f2db3383b
Blen Wondimagegn <mekuriablen@gmail.com>
2020-10-04 08:12:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,24 +18,24 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    # # user = User.new(:username => params[:username], :password => params[:password])
-    # user = params[:username]
-    # pass = params[:password]
-    # if session[:user_id]
-    #   user = User.new(:username => params[:username], :password => params[:password])
-    #   redirect "/login"
-    # else !!session[:user_id]
-    #   redirect "/failure"
+    # user = User.new(:username => params[:username], :password => params[:password])
+    user = params[:username]
+    pass = params[:password]
+    if session[:user_id]
+      user = User.new(:username => params[:username], :password => params[:password])
+      redirect "/login"
+    else !!session[:user_id]
+      redirect "/failure"
       
-    # end
-    if params[:username] == "" || params[:password] == ""
-      redirect to "/failure"
-    else
-    user = User.create(:username => params[:username], :password => params[:password])
-
-      redirect to "/login"
     end
-  end
+  #   if params[:username] == "" || params[:password] == ""
+  #     redirect to "/failure"
+  #   else
+  #   user = User.create(:username => params[:username], :password => params[:password])
+
+  #     redirect to "/login"
+  #   end
+  # end
 
   get '/account' do
     @user = User.find(session[:user_id])
