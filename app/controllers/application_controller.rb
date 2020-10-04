9d00003f2c68ca4737bf55582aefc08f6393101b
Blen Wondimagegn <mekuriablen@gmail.com>
2020-10-04 07:37:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
     # # user = User.new(:username => params[:username], :password => params[:password])
     # user = params[:username]
     # pass = params[:password]
-    if params[:username] == "" || params[:password] == ""
+    if params[:username] == nil || params[:password] == nil
        redirect "/failure"
     else 
       user = User.new(:username => params[:username], :password => params[:password])
