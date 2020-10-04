46c37ac6b48bbfde5bec7c952d9e88f4868fef84
Blen Wondimagegn <mekuriablen@gmail.com>
2020-10-04 07:54:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,6 +56,17 @@ class ApplicationController < Sinatra::Base
     # else
     #   redirect "/failure"
     # end
+
+    @user = User.find_by(username: params[:username])
+     if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+
+       redirect to "/account"
+     else
+
+
+       redirect to "/failure"
+     end
   end
 
   get "/failure" do
