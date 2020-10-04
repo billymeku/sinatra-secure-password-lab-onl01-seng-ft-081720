2af24394b19e07bf4cbac81e365bc871d53397d2
Blen Wondimagegn <mekuriablen@gmail.com>
2020-10-04 06:42:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-    user = User.new(:username => params[:username], :password => params[:password])
+    # user = User.new(:username => params[:username], :password => params[:password])
     if user.save
       redirect "/login"
     else
@@ -38,13 +38,13 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
-    user = User.find_by(:username => params[:username])
+    # user = User.find_by(:username => params[:username])
    
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/success"
     else
-      redirect "/login"
+      redirect "/failure"
     end
   end
 
