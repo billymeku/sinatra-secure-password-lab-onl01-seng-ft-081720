f656c1ffd1f8f861825125d8fec4b4c034ec77f0
Blen Wondimagegn <mekuriablen@gmail.com>
2020-10-04 06:34:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,14 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
+    user = User.new(:username => params[:username], :password => params[:password])
+ 
+    if user.save
+      redirect "/login"
+    else
+      redirect "/failure"
+  end
+    
 
   end
 
@@ -33,6 +41,8 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    
+    
   end
 
   get "/failure" do
